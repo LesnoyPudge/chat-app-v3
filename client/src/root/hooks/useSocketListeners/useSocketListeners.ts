1fb97179e6231a5167938e7cfe7976f6c02bdfd9
LesnoyPudge <chuykin.a01@gmail.com>
2023-08-28 08:03:54 +0500

    'update'

--- client/src/root/hooks/useSocketListeners/useSocketListeners.ts
+++ client/src/root/hooks/useSocketListeners/useSocketListeners.ts
@@ -2,93 +2,49 @@ import { UserSlice, ChannelSlice, ChatSlice, MessageSlice, PrivateChannelSlice, 
 import { useAppDispatch } from '@redux/hooks';
 import { socketIO } from '@root/features';
 import { useEffect } from 'react';
-import { SUBSCRIBABLE_ENTITIES, SOCKET_SERVER_EVENT_NAMES, toSocketEventName, AnyFunction } from '@shared';
+import { SUBSCRIBABLE_ENTITIES, SOCKET_SERVER_EVENT_NAMES, toSocketEventName } from '@shared';
 
 
+
+const slices = {
+    [SUBSCRIBABLE_ENTITIES.CHANNEL]: ChannelSlice,
+    [SUBSCRIBABLE_ENTITIES.CHAT]: ChatSlice,
+    [SUBSCRIBABLE_ENTITIES.MESSAGE]: MessageSlice,
+    [SUBSCRIBABLE_ENTITIES.PRIVATE_CHANNEL]: PrivateChannelSlice,
+    [SUBSCRIBABLE_ENTITIES.ROLE]: RoleSlice,
+    [SUBSCRIBABLE_ENTITIES.ROOM]: RoomSlice,
+    [SUBSCRIBABLE_ENTITIES.USER]: UserSlice,
+};
 
 export const useSocketListeners = () => {
     const { dispatch } = useAppDispatch();
 
-    // const listeners = Object.keys(SUBSCRIBABLE_ENTITIES).reduce((acc, cur) => {
-    //     return acc;
-    // }, {} as Record<keyof typeof SUBSCRIBABLE_ENTITIES, AnyFunction>);
-
-    // Object.keys(SUBSCRIBABLE_ENTITIES).forEach((key) => {
-    //     socketIO.on(
-    //         toSocketEventName(SUBSCRIBABLE_ENTITIES[key], SOCKET_SERVER_EVENT_NAMES.DATA),
-    //         (_, data) => dispatch(UserSlice.actions.upsertOne(data)),
-    //     );
-    // })
-
     useEffect(() => {
-        socketIO.on(
-            toSocketEventName(SUBSCRIBABLE_ENTITIES.USER, SOCKET_SERVER_EVENT_NAMES.DATA),
-            (_, data) => dispatch(UserSlice.actions.upsertOne(data)),
-        );
+        Object.keys(SUBSCRIBABLE_ENTITIES).forEach((key) => {
+            const value = SUBSCRIBABLE_ENTITIES[key];
 
-        socketIO.on(
-            toSocketEventName(SUBSCRIBABLE_ENTITIES.CHANNEL, SOCKET_SERVER_EVENT_NAMES.DATA),
-            (_, data) => dispatch(ChannelSlice.actions.upsertOne(data)),
-        );
+            socketIO.on(
+                toSocketEventName(value, SOCKET_SERVER_EVENT_NAMES.DATA),
+                (_: string, entity: any) => {
+                    dispatch(slices[value].actions.upsertOne(entity));
+                },
+            );
 
-        socketIO.on(
-            toSocketEventName(SUBSCRIBABLE_ENTITIES.CHAT, SOCKET_SERVER_EVENT_NAMES.DATA),
-            (_, data) => dispatch(ChatSlice.actions.upsertOne(data)),
-        );
+            socketIO.on(
+                toSocketEventName(value, SOCKET_SERVER_EVENT_NAMES.DELETE),
+                (id) => {
+                    dispatch(slices[value].actions.removeOne(id));
+                },
+            );
+        });
 
-        socketIO.on(
-            toSocketEventName(SUBSCRIBABLE_ENTITIES.MESSAGE, SOCKET_SERVER_EVENT_NAMES.DATA),
-            (_, data) => dispatch(MessageSlice.actions.upsertOne(data)),
-        );
+        return () => {
+            Object.keys(SUBSCRIBABLE_ENTITIES).forEach((key) => {
+                const value = SUBSCRIBABLE_ENTITIES[key];
 
-        socketIO.on(
-            toSocketEventName(SUBSCRIBABLE_ENTITIES.PRIVATE_CHANNEL, SOCKET_SERVER_EVENT_NAMES.DATA),
-            (_, data) => dispatch(PrivateChannelSlice.actions.upsertOne(data)),
-        );
-
-        socketIO.on(
-            toSocketEventName(SUBSCRIBABLE_ENTITIES.ROLE, SOCKET_SERVER_EVENT_NAMES.DATA),
-            (_, data) => dispatch(RoleSlice.actions.upsertOne(data)),
-        );
-
-        socketIO.on(
-            toSocketEventName(SUBSCRIBABLE_ENTITIES.ROOM, SOCKET_SERVER_EVENT_NAMES.DATA),
-            (_, data) => dispatch(RoomSlice.actions.upsertOne(data)),
-        );
-
-        socketIO.on(
-            toSocketEventName(SUBSCRIBABLE_ENTITIES.USER, SOCKET_SERVER_EVENT_NAMES.DELETE),
-            (id) => dispatch(UserSlice.actions.removeOne(id)),
-        );
-
-        socketIO.on(
-            toSocketEventName(SUBSCRIBABLE_ENTITIES.CHANNEL, SOCKET_SERVER_EVENT_NAMES.DELETE),
-            (id) => dispatch(ChannelSlice.actions.removeOne(id)),
-        );
-
-        socketIO.on(
-            toSocketEventName(SUBSCRIBABLE_ENTITIES.CHAT, SOCKET_SERVER_EVENT_NAMES.DELETE),
-            (id) => dispatch(ChatSlice.actions.removeOne(id)),
-        );
-
-        socketIO.on(
-            toSocketEventName(SUBSCRIBABLE_ENTITIES.MESSAGE, SOCKET_SERVER_EVENT_NAMES.DELETE),
-            (id) => dispatch(MessageSlice.actions.removeOne(id)),
-        );
-
-        socketIO.on(
-            toSocketEventName(SUBSCRIBABLE_ENTITIES.PRIVATE_CHANNEL, SOCKET_SERVER_EVENT_NAMES.DELETE),
-            (id) => dispatch(PrivateChannelSlice.actions.removeOne(id)),
-        );
-
-        socketIO.on(
-            toSocketEventName(SUBSCRIBABLE_ENTITIES.ROLE, SOCKET_SERVER_EVENT_NAMES.DELETE),
-            (id) => dispatch(RoleSlice.actions.removeOne(id)),
-        );
-
-        socketIO.on(
-            toSocketEventName(SUBSCRIBABLE_ENTITIES.ROOM, SOCKET_SERVER_EVENT_NAMES.DELETE),
-            (id) => dispatch(RoomSlice.actions.removeOne(id)),
-        );
+                socketIO.off(toSocketEventName(value, SOCKET_SERVER_EVENT_NAMES.DATA));
+                socketIO.off(toSocketEventName(value, SOCKET_SERVER_EVENT_NAMES.DELETE));
+            });
+        };
     }, [dispatch]);
 };
